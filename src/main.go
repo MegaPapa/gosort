4afd272a141cb322ca7b03c475befbf1ae35e681
Maxim Petrusevich <maks1464@gmail.com>
2017-08-20 00:12:29 +0300

    quicksort init

--- src/main.go
+++ src/main.go
@@ -1,22 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"./sorts"
+)
 
 func main() {
-	a := 10
-	b := 3
-	swap(&a, &b);
-	fmt.Printf("%d - %d", a, b)
-	/*var matrix = []int {
-		1, 2, 3,
-		4, 10, 6,
-		7, 8, 9,
+	var array [10]int
+	fmt.Println(array)
+	arr(array[:])
+	fmt.Println(array)
+	quicksort(array) 
+}
+
+func arr(array []int) {
+	for i := 0; i < len(array); i++ {
+		array[i]++
 	}
-	var maxElement = matrix[0]
-    for _, element := range matrix {
-		if element > maxElement {
-			maxElement = element
-		}
-	}
-	fmt.Println(matrix)*/
 }
---
+++ src/sorts/quicksort.go
@@ -0,0 +1,11 @@
+package sorts
+
+import (
+	"fmt"
+)
+
+func quicksort(array []int) {
+	/*currentSize := len(array)
+	middleElement := array[int(currentSize / 2)]*/
+	fmt.Println("govno")
+}
